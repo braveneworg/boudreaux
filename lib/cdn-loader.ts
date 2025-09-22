6709dee6e93c273ae9798a4fe7d388d50e0a838f
Michaux Kelley <michauxkelley@gmail.com>
2025-09-22 00:20:23 -0400

    Remove CDN domain environment variable and associated loader function

--- lib/cdn-loader.ts
+++
@@ -1,5 +0,0 @@
-'use client'; // Required for custom loader files in App Router
-
-export default function loader({ src, width, quality }: { src: string; width: number; quality?: number }) {
-  return `https://${process.env.NEXT_APP_CDN_DOMAIN}/public/${src}?w=${width}&q=${quality || 75}`;
-}
