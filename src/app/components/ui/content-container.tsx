2c70fac8d995a94568aa0d3960f9e5d3f7546318
Michaux Kelley <michauxkelley@gmail.com>
2025-11-16 12:22:50 -0500

    refactor: Update UI components and styles based on design feedback (#52)

--- src/app/components/ui/content-container.tsx
+++ src/app/components/ui/content-container.tsx
@@ -11,7 +11,7 @@ export const ContentContainer = ({ children, className }: ContentContainerProps)
   return (
     <section
       className={cn(
-        'flex-1 font-sans bg-zinc-100 border-t-[1px] border-t-zinc-300 min-h-full flex flex-col w-full pt-2 bg-[url(/media/fake-four-hand-outline-and-text-1.png)] bg-fixed bg-repeat',
+        'flex-1 font-sans bg-zinc-100 border-t-[1px] border-t-zinc-300 min-h-full flex flex-col w-full pt-2',
         className
       )}
       key="client-only"
