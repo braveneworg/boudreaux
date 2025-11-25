d82731b8307e5847ca20270709688b4abaf875a6
Michaux Kelley <michauxkelley@gmail.com>
2025-11-25 09:05:36 -0500

    chore: update dependencies and improve database schema
    - Updated various package versions in package.json for better
    performance and security.
    - Added indexes to Account and Session models in schema.prisma for optimized queries.
    - Introduced new models: Image, Group, Url, Genre, Label, Artist, ArtistGroup, ArtistLabel, ArtistRelease, ArtistUrl, Tag, Release, Track, ReleaseTrack, and Instrument to enhance data structure.
    - Refactored FeaturedArtistsThumbCarousel component to simplify image source retrieval.

--- src/app/components/ui/audio/featured-artist-thumb-carousel.tsx
+++ src/app/components/ui/audio/featured-artist-thumb-carousel.tsx
@@ -11,17 +11,12 @@ import {
 export function FeaturedArtistsThumbCarousel({ artists }: { artists: Artist[] }) {
   return (
     <Carousel aria-label="Featured Artists" orientation="horizontal">
-      <CarouselContent>
+      <CarouselContent className="flex justify-center gap-2">
         {artists.map((artist) => (
           <CarouselItem key={artist.id}>
             <Image
               className="border-radius-[0.5rem]"
-              src={
-                artist.releases.sort(
-                  (a: { releasedOn: number }, b: { releasedOn: number }) =>
-                    b.releasedOn - a.releasedOn
-                )[0].coverArt
-              }
+              src={artist.releases.sort((a, b) => b.releasedOn - a.releasedOn)[0].coverArt}
               alt={artist.name}
               width={84}
               height={84}
