25d10d857e5d6a1e5eb41bb4996d83c4abbcbef9
Michaux Kelley <michauxkelley@gmail.com>
2026-02-14 19:36:25 -0500

    Add copyleft MPL 2.0 license to project

--- src/app/admin/featured-artists/new/page.tsx
+++ src/app/admin/featured-artists/new/page.tsx
@@ -1,3 +1,6 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import FeaturedArtistForm from '@/app/components/forms/featured-artist-form';
 
 export default function NewFeaturedArtistPage() {
